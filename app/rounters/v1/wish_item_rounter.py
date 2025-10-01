e1c96877d8b1e5e7af10ea799301fd836696e2ae
Charif <hifrook.zen5@gmail.com>
2025-10-01 23:58:55 +0700

    wish item checked

--- app/rounters/v1/wish_item_rounter.py
+++ app/rounters/v1/wish_item_rounter.py
@@ -5,6 +5,9 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from sqlalchemy.orm import Session
+
+from app.db.models.items.item import Item
+from app.schemas.item_schema import ItemResponse
 from ...db.database import get_db
 
 from ...db.models.items.wishItem import WishItem
@@ -20,6 +23,12 @@ async def add_wish_item(
     db : Session = Depends(get_db),
     current_user: dict = Depends(get_current_user)
 ):
+    
+    existing_wish_item_db = db.query(WishItem).filter(WishItem.item_id == wish.item_id, WishItem.user_id == current_user["id"]).first()
+
+    if existing_wish_item_db :
+        raise HTTPException(status_code=403, detail="item already in wish list")
+
     db_wish = WishItem(
         user_id = current_user["id"],
         item_id = wish.item_id
@@ -30,13 +39,13 @@ async def add_wish_item(
     db.refresh(db_wish)
     return db_wish
 
-@rounter.delete("/my/{item_id}")
+@rounter.delete("/my/{wish_id}")
 async def remove_wish_item(
-    item_id: int,
+    wish_id: int,
     db : Session = Depends(get_db),
     current_user: dict = Depends(get_current_user) 
 ):
-    db_wish_item = db.query(WishItem).filter(WishItem.id == item_id, WishItem.user_id == current_user["id"]).first()
+    db_wish_item = db.query(WishItem).filter(WishItem.id == wish_id, WishItem.user_id == current_user["id"]).first()
     if not db_wish_item:
         raise HTTPException(status_code=404, detail="Item not found")
     
@@ -63,6 +72,19 @@ async def set_privacy_wish_list(
     db.refresh(db_wish)
     return db_wish
 
+@rounter.get("/my/items", response_model=List[ItemResponse])
+async def get_my_wish_list(
+    skip: int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db),
+    current_user : dict = Depends(get_current_user)
+):
+    items = db.query(Item).join(WishItem, Item.id == WishItem.item_id)\
+                              .filter(WishItem.user_id == current_user["id"])\
+                              .offset(skip).limit(limit).all()
+                              
+    return items
+
 @rounter.get("/my", response_model=List[WishItemResponse])
 async def get_my_wish_list(
     skip: int = 0,
@@ -73,10 +95,18 @@ async def get_my_wish_list(
     wish_list = db.query(WishItem).filter(WishItem.user_id == current_user["id"]).offset(skip).limit(limit).all()
     return wish_list
 
-# @rounter.get("/my/share", response_model=List[WishItemResponse])
-# async def share_my_wish_List(
-#     skip : int = 0,
-#     limit : int = 10,
-#     db: Session = Depends(get_db),
+
+
+@rounter.get("/my/share/{target_id}", response_model=List[ItemResponse])
+async def share_my_wish_List(
+    target_id : int,
+    skip : int = 0,
+    limit : int = 10,
+    db: Session = Depends(get_db),
+):
+    wish_items_db = db.query(Item).join(WishItem, Item.id == WishItem.item_id)\
+                                  .filter(WishItem.user_id == target_id, 
+                                          WishItem.privacy == WishPrivacy.PUBLIC.value
+                                        ).offset(skip).limit(limit).all()
     
-# )
+    return wish_items_db
